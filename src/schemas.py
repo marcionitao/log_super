b15ae1e000ce0611b1a9005d385b564853aef5e2
Márcio <marcionitao@gmail.com>
2026-01-20 19:54:26 +0100

    Add user schema and endpoint for user creation

--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+
+
+# requição
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+# response
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
