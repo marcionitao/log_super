8413f4875b3a91b07e7d1f531cb0d45a5ae7d9d2
Márcio <marcionitao@gmail.com>
2026-01-27 22:46:33 +0100

    Add test for user creation endpoint with validation checks

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,26 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert(validação)
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user_deve_retornar_created():
+    # Arrange(organização)
+    client = TestClient(app)
+
+    # Act(ação) -> UserSchema(Requisição)
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@mail.com',
+            'password': 'secret',
+        },
+    )
+
+    # Assert(validação) -> UserPublic(Resposta)
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@mail.com',
+        'id': 1,
+    }
