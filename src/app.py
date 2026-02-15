5e72aa0b29c8214b3488fb45e4f0ebcb566c1d86
Márcio <marcionitao@gmail.com>
2026-02-15 16:51:33 +0100

    Add update user endpoint and implement user retrieval in user listing

--- src/app.py
+++ src/app.py
@@ -9,11 +9,13 @@ app = FastAPI()
 database = []
 
 
+# mensagem
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
 
 
+# cria usuarios
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
     # Crie um usuário no formato do banco(array), copiando todos os dados recebidos na requisição e adicionando um id novo.
@@ -24,6 +26,22 @@ def create_user(user: UserSchema):
     return user_with_id
 
 
+# lista usuarios
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
 def read_users():
     return {'users': database}
+
+
+# editar usuario
+@app.put(
+    '/users/{user_id}',
+    status_code=HTTPStatus.CREATED,
+    response_model=UserPublic,
+)
+def update_user(user_id: int, user: UserSchema):
+    # Busca um usuário no formato do banco(array), copiando todos os dados recebidos na requisição e adicionando um id novo.
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+    # atualizar a lista
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
