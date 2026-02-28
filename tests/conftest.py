e67a39d2fcfff805ca76acb3ba064c008c30fd2a
Márcio <marcionitao@gmail.com>
2026-02-28 17:55:19 +0100

    Add session fixture for database testing and implement user creation test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,29 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from src.app import app
+from src.models import table_registry
 
 
 # função para evitar repetição e aproveitar trecho de codigo
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    # "create_engine", cria um ponto de contato com o DB, estabelece e gere as conexões(credenciais, uri do DB)
+    engine = create_engine('sqlite:///:memory:')  # DB em memoria
+
+    # enquanto fores rodar estes testes cria toda a estrutura do BD para mim.
+    table_registry.metadata.create_all(engine)
+
+    # a "Session" faz o meio campo entre a nossa app e o DB
+    with Session(engine) as session:
+        yield session
+
+    # destrua tudo assim que terminar os testes
+    table_registry.metadata.drop_all(engine)
---
+++ tests/test_db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import select
+
+from src.models import User
+
+
+def test_create_user(session):
+
+    # session (conftest.py) => Arrange(organização)
+
+    user = User(username='Marcio', email='marcio@mail.com', password='12345')
+    # adicionando o 'user' a sessão
+    session.add(user)
+    session.commit()
+    # Scalar possibilita fazer uma query no DB injetando SQL comandos e retorna um Objeto
+    result = session.scalar(
+        select(User).where(User.email == 'marcio@mail.com')
+    )
+
+    assert result.username == 'Marcio'
